674aa0826d5b6f1f56f1e6017772ab55e2aaf677
DrKillex <lcollaro1994@gmail.com>
2023-05-22 17:18:06 +0200

    small fix

--- resources/views/admin/records/show.blade.php
+++ resources/views/admin/records/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <ul class="list-unstyled">
             <li><h2>title: {{$record->title}}</h2></li>
-            <li><h3>type: <a href="{{ route('admin.types.show', $record->type) }}">{{ $record->type?->name ?: 'Nessuna tipologia' }}</a></h3></li>
+            <li><h3>type: <a href="@if ($record->type){{ route('admin.types.show', $record->type) }}@endif">{{ $record->type?->name ?: 'Nessuna tipologia' }}</a></h3></li>
             <li>creation date: {{$record->creation_date}}</li>
             <li>description: {{$record->record_description}}</li>
             <li>completed: {{$record->completed}}</li>
